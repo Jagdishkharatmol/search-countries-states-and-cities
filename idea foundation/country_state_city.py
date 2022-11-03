06174052f2ed71bd0089bee581092775420334a6
Jk <44578562+Jagdishkharatmol@users.noreply.github.com>
2022-11-03 07:07:56 +0530

    Add files via upload

---
+++ idea foundation/country_state_city.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flaskext.mysql import MySQL
+import json
+import ast
+ 
+ 
+app = Flask(__name__)
+app.secret_key = 'ideafoundation'
+ 
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'idea'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+
+try:
+    conn = mysql.connect()
+    cursor =conn.cursor()
+except Exception as e:
+    print("Error:",e)
+
+
+
+
+@app.route('/',methods=["GET"])
+def show_data():
+    fetch_countries_data=f"""SELECT id, name, iso3, iso2, numeric_code, phone_code, capital,
+                               currency, currency_name, currency_symbol, tld, native,
+                               region, subregion, timezones, translations, latitude,
+                               longitude, emoji, emojiU FROM country_info"""
+    cursor.execute(fetch_countries_data)
+    global countries
+    countries = cursor.fetchall()
+    #cursor.close()
+    return render_template('index.html',countries=countries)
+
+
+@app.route('/state/<string:id>/',methods=["GET",'POST'])
+def fill_data_state(id):
+    fetch_state_data=f"select states from country_info where id={id}";
+    cursor.execute(fetch_state_data)
+    global states
+    states = cursor.fetchall()
+    states=states[0][0]
+    states=ast.literal_eval(states)
+    return render_template('index.html',states=states,countries=countries)
+
+@app.route('/city/<string:id>/',methods=["GET",'POST'])
+def fill_data_city(id):
+    global cities
+    cities=states[int(id)]['cities']
+    return render_template('index.html',states=states,countries=countries,cities=cities)
+    
+
+
+if __name__=='__main__':
+    app.run(debug=False)
+
---
+++ idea foundation/json_data_insert.py
@@ -0,0 +1,47 @@
+import json
+from urllib.request import urlopen
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+
+link_to_data='https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries%2Bstates%2Bcities.json'
+
+class InsertDataInDatabase:
+  def __init__(self,link,creds):
+    self.link=link
+    self.creds=creds
+
+  def fetch_data(self):
+    response = urlopen(self.link)
+    db_data=json.loads(response.read())
+    return db_data
+
+  def insert_data(self):
+    try: 
+        engine = create_engine('mysql+pymysql://root:root@localhost/idea')
+    except Exception as e:
+          engine.dispose()
+          print("Error",e)
+          return 0
+    
+    print("Database connected !")
+    
+    try:
+        db_data=InsertDataInDatabase.fetch_data(self)
+        df_country_info = pd.DataFrame.from_dict(db_data)
+        df_country_info = df_country_info.applymap(str)
+        df_country_info.to_sql('country_info',if_exists='append',con=engine)
+    except Exception as e:
+          engine.dispose()
+          print("Error",e)
+          return 0
+        
+    print(f'Inserted records in database !')
+
+    return 1
+
+
+
+insert_data_obj=InsertDataInDatabase(link_to_data,creds)
+insert_data_obj.insert_data()
